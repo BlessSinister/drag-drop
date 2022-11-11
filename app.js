084fbdaa3b3e984b14ab15379edb196291be321d
Bless <ig.marks@yandex.ru>
2022-11-11 22:45:45 +0300

    2-reparing

--- app.js
+++ app.js
@@ -1,5 +1,7 @@
 const item = document.querySelector('.item')
 const placeholders = document.querySelectorAll('.placeholder')
+const holders = ['hold1', 'hold2', 'hold3']
+
 
 for (placeholder of placeholders) {
     placeholder.addEventListener('dragover', dragover)
@@ -12,25 +14,31 @@ item.addEventListener('dragstart', dragStart)
 item.addEventListener('dragend', dragEnd)
 
 function dragStart(event) {
-    event.target.classList.add('hold')
+    setTimeout(() => {
+        event.target.classList.add(holders[Math.floor(Math.random() * 3)])
+    }, 1500)
+
     setTimeout(() => {
         event.target.classList.add('hide')
     }, 0);
 
 }
 function dragEnd(event) {
-    event.target.classList.remove('hold', 'hide')
+    event.target.classList.remove('hold1', 'hold2', 'hold3', 'hide')
 }
 function dragover(event) {
     event.preventDefault()
 }
 function dragenter(event) {
     event.target.classList.add('hovered')
+
 }
 function dragleave(event) {
     event.target.classList.remove('hovered')
+
 }
 function drop(event) {
     event.target.append(item)
     event.target.classList.remove('hovered')
+
 }
